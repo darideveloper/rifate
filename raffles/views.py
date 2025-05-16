4d73cb25025c15ff666e439014e426b1b8c42549
Abel Soto Valdez <abelsotovaldez@MacBook-Air-de-Abel.local>
2025-05-15 23:50:26 -0600

    feat(templates): raffle information on index and boletos

--- raffles/views.py
+++ raffles/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.views import View
 
+from .models import Raffle, Ticket
+
+
 # Create your views here.
 class HomeView(View):
     def get(self, request):
-        return render(request, "raffles/index.html")
-    
+        raffle = Raffle.objects.all().first()
+        return render(request, "raffles/index.html", {
+            "raffle": raffle}
+        )
+
+
 class TicketsView(View):
     def get(self, request):
-        return render(request, "raffles/boletos.html")
+        raffle = Raffle.objects.all().first()
+        tickets = Ticket.objects.all()
+        return render(request, "raffles/boletos.html",{
+            "raffle": raffle,
+            "tickets":tickets}
+        )
